d88f1eea632757a2210cb2f0a0d6c0629da115e4
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-05 09:59:09 -0300

    1.1-into.py

--- 1.1-into.py
+++ 1.1-into.py
@@ -1,4 +1,17 @@
 '''
+NumPy - Numeric Python
+    Extension module for Python
+    Mostly written in C. (speed)
+    Powerful data structures (Multidimensional arrays and matrices)
+
+Scipy - Scientific Python
+    Needs NumPy
+    Useful function for:
+        Minimization, Regression, Fourier-transformation (and others)
+'''
+
+
+'''
     Simple Exemple demonstrate how we cab use Numpy and Matplotlib
     '''
 
@@ -32,3 +45,46 @@ import matplotlib.pyplot as plt
 
 plt.plot(C)
 plt.show()
+
+
+'''
+    Memory Consumption: NDARRAY and list
+
+    using sys
+    '''
+
+from sys import getsizeof as size
+
+lst = [24, 12, 57]
+size_of_list_object = size(lst) # only green box
+size_of_elements = len(lst) * size(lst[0]) # 24, 12, 57
+total_list_size = size_of_list_object + size_of_elements
+print("Size without the size of the elements: ", size_of_list_object)
+print("Size of all the elements: ", size_of_elements)
+print("Total size of list, including elements: ", total_list_size)
+
+'''
+    In previosly exemplo, we made the assimption that all the integer elements of our list have the same size.
+    Of course, this is not valid in general, because memory consumpion will be higher for larger integers.
+'''
+print("one more element")
+lst = [24, 12, 57, 42]
+size_of_list_object = size(lst) # only green box
+size_of_elements = len(lst) * size(lst[0]) # 24, 12, 57
+total_list_size = size_of_list_object + size_of_elements
+print("Size without the size of the elements: ", size_of_list_object)
+print("Size of all the elements: ", size_of_elements)
+print("Total size of list, including elements: ", total_list_size)
+
+
+print("Empyt list")
+lst = []
+size_of_list_object = size(lst) # only green box
+size_of_elements = len(lst) * size(lst[0]) # 24, 12, 57
+total_list_size = size_of_list_object + size_of_elements
+print("Size without the size of the elements: ", size_of_list_object)
+print("Size of all the elements: ", size_of_elements)
+print("Total size of list, including elements: ", total_list_size)
+
+#pag. 15/514
+#file:///L:/Linear_/Base-TI/bernd_klein_python_data_analysis_a4.pdf
